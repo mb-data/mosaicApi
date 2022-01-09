e302a0dee8d5d5bc29278ea83cc5f300953225ac
andre.insardi <andre.insardi@gmail.com>
2022-01-09 17:32:58 -0300

    adicona cors3

--- src/app.py
+++ src/app.py
@@ -7,13 +7,19 @@ from src.config.settings import config_by_name
 from src.models import db
 #import blueprint controllers
 from src.api.lead_controller import ns as lead_namespace
-
+from flask_cors import CORS
     
 def create_app(config_name):
     #inicia flask
     app = Flask(__name__)
     #inicia configuracao variaveis de ambiente
+    CORS(app, supports_credentials=True)
+ 
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    
     app.config.from_object(config_by_name[config_name])
+
     setup_app(app)
 
     return app
